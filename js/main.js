1a75a6e3204db5af7459a15b5c1f3fb23e635687
Mike Swatek <mike.swatek@synergy55.com>
2015-03-25 14:14:43 -0500

    Models Exercise

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+var Vehicle = Backbone.Model.extend({
+  idAttribute: 'registrationNumber',
+  urlRoot:     '/api/vehicles/',
+  defaults: {
+    registrationNumber: ""
+  },
 
+  start: function() {
+    console.log('Vehicle Started');
+  },
+  validate: function(attrs){
+    if (!attrs.registrationNumber)
+        return "Registration Number is required!";
+  }
+});
+var Car = Vehicle.extend({
+  start: function() {
+    console.log('Car with registration number ' + this.get("registrationNumber") + ' started.');
+  }
+});
+var corolla = new Car({registrationNumber: "XLI887", color: "blue"});
+corolla.start();
